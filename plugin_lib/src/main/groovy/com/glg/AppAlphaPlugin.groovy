6ee1f65756be26525b541c78936cef83988cd2bc
郭林广 <guoguanglin@tyche.net.cn>
2020-06-28 10:46:38 +0800

    升级gradle2

--- plugin_lib/src/main/groovy/com/glg/AppAlphaPlugin.groovy
+++ plugin_lib/src/main/groovy/com/glg/AppAlphaPlugin.groovy
@@ -15,20 +15,27 @@ class AppAlphaPlugin implements Plugin<Project>{
         if (project.android.hasProperty("applicationVariants")) {
             project.android.applicationVariants.all { variant ->
                 if ("debug" != variant.name) {//防止debug时，手机不能安装app
-                    def flag = variant.versionName.replaceAll("\\.", '')
-                    def outputPath = project.getRootProject().getProjectDir().getAbsolutePath() + "/app/" + variant.name + "/v" + flag
-                    def outputName = project.getRootProject().getName() + "_v${flag}${variant.name}.apk"
-                    variant.getPackageApplicationProvider().get().outputDirectory = new File(outputPath)
-                    variant.getPackageApplicationProvider().get().outputScope.apkDatas.forEach { apkData ->
-                        apkData.outputFileName =outputName
+                    variant.outputs.each {output ->
+                        File file = output.outputFile
+                        print(file.getName())
+
                     }
-                    def upCaseName = variant.name.substring(0, 1).toUpperCase() + variant.name.substring(1)
-                    project.tasks.create("uploadApk" + upCaseName,PublishApkTask.class) {
-                        setApkOutputPath(outputPath)
-                        setApkOutputName(outputName)
-                        dependsOn("assemble" + upCaseName)
-                        setGroup("publish")
-                    }
+
+//                    def flag = variant.versionName.replaceAll("\\.", '')
+//                    def outputPath = project.getRootProject().getProjectDir().getAbsolutePath() + "/app/" + variant.name + "/v" + flag
+//                    def outputName = project.getRootProject().getName() + "_v${flag}${variant.name}.apk"
+//                    variant.getPackageApplicationProvider().get().outputDirectory = new File(outputPath)
+//                    variant.getPackageApplicationProvider().get().outputScope.apkDatas.forEach { apkData ->
+//                        apkData.outputFileName =outputName
+//                    }
+//                    variant.getPackageApplicationProvider().properties
+//                    def upCaseName = variant.name.substring(0, 1).toUpperCase() + variant.name.substring(1)
+//                    project.tasks.create("uploadApk" + upCaseName,PublishApkTask.class) {
+//                        setApkOutputPath(outputPath)
+//                        setApkOutputName(outputName)
+//                        dependsOn("assemble" + upCaseName)
+//                        setGroup("publish")
+//                    }
                 }
 
             }
